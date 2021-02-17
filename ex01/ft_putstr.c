403b77bddb4b1119b511eca16f4870b8cc4d241a
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-17 06:41:49 -0500

    ex01 works main--

--- ex01/ft_putstr.c
+++ ex01/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 15:06:44 by tblanco           #+#    #+#             */
-/*   Updated: 2021/02/16 15:08:12 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/02/17 06:41:40 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,56 @@
 ** pointeur passé en paramètre à la fonction.
 */
 
-void ft_putstr(char *str)
-{
+#include <unistd.h>
 
+int	ft_strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while(*str++)
+		len++;
+	return (len);
 }
 
+void ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
+}
+
+#include <stdio.h>
+#include <string.h>
 int	main()
 {
-	
+	// TEST_1
+		char str_1[] = "Bonjour 42";
+		printf("TEST_o_1 -- string: %s -- TEST_1\n", str_1);
+		ft_putstr(str_1);
+
+	// TEST_1
+		char str_2[] = "";
+		printf("\n\nTEST_o_2 -- string: %s -- TEST_2\n", str_2);
+		ft_putstr(str_2);
+
+	// TEST_3
+		char str_3[] = "  askoasd\t";
+		printf("\n\nTEST_o_3 -- string: %s -- TEST_3\n", str_3);
+		ft_putstr(str_3);
+
+	// TEST_4
+		char str_4[] = "!@#!@%ˆ&*(_+:{}<>,.';";
+		printf("\n\nTEST_o_4 -- string: %s -- TEST_4\n", str_4);
+		ft_putstr(str_4);
+
+	// TEST_5
+		char str_5[] = "Hello 42        ";
+		printf("\n\nTEST_o_5 -- string: %s -- TEST_5\n", str_5);
+		ft_putstr(str_5);
+
+		
+	// END
+		printf("\n\n************** END **************\n\n");
+
+		
+	return (0);
 }
