8a45668786d33a40b66d5070e78accaa4dc9a3a8
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-19 11:11:34 -0500

    ex05 works test--

--- ex05/ft_atoi_base.c
+++ ex05/ft_atoi_base.c
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 15:19:55 by tblanco           #+#    #+#             */
-/*   Updated: 2021/02/17 11:50:10 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/02/19 11:05:22 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,41 +33,6 @@ int		ft_strlen(char *str)
 	while(*str++)
 		len++;
 	return (len);
-}
-
-int		str_to_int(char *str)
-{
-	int pow;
-	int nb;
-
-	pow = 1;
-	nb = 0;
-	while(*--str >= '0' && *str <= '9')
-	{	
-		nb += (*str - '0') * pow;
-		pow *= 10;
-	}
-	return (nb);
-}
-
-int		ft_atoi(char *s)
-{
-	int i;
-	int neg_pos;
-
-	i = -1;
-	neg_pos = 1;
-	while (s[++i] && (s[i] == ' ' || (s[i] >= '\t' && s[i] <= '\r')))
-		continue;
-	while (s[++i] == '-' || s[i] == '+')
-	{
-		if (s[i] == '-')
-			neg_pos *= -1;
-	}
-	while (s[++i] >= '0' && s[i] <= '9')
-		continue;
-	return (str_to_int(&s[i]));
-	
 }
 
 int 	sames_char(char *base)
@@ -104,24 +69,83 @@ int		prohibited_char(char *base)
 	return (0);
 }
 
-int		base_to_str(char *str, char *base)
+int		is_in_base(char c, char *base)
 {
+	int i;
+	
+	i = -1;
+	while(base[++i])
+		if (c == base[i])
+			return (1);
+	return(0);
+}
 
+int		indice_in_base(char c, char *base)
+{
+	int i;
+	
+	i = -1;
+	while(base[++i])
+		if (c == base[i])
+			return (i);
+	return(0);
+}
+
+int ft_power(int nb, int power)
+{
+        int ret;
+
+        ret = nb;
+        if (power < 0)
+            return (0);
+        if (power == 0)
+            return (1);
+        if (power == 1)
+            return (nb);
+        else
+            return (nb * ft_power(nb, power - 1));
 }
 
 int ft_atoi_base(char *str, char *base)
 {
-	if (ft_strlen(base) <= 1 || ft_strlen(str) <= 0)
+	int i;
+	int neg;
+	int len_base;
+	int ret;
+	int pow;
+	
+	i = 0;
+	len_base = ft_strlen(base);
+	neg = 1;
+	ret = 0;
+	pow = 0;
+	if (len_base <= 1 || ft_strlen(str) <= 0)
 		return (0);
-	else if (sames_char(base))
-		return (0);
-	else if (prohibited_char(base))
-		return (0);
-	else
-		base_to_str(str, base);
+	while ((str[i] >= '\t' && str[i] <= '\r') || (str[i] == ' '))
+		i++;
+	while ((str[i] == '-') || (str[i] == '+'))
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (is_in_base(str[i], base))
+	{
+		i++;
+	}
+	i--;
+	while (is_in_base(str[i], base))
+	{
+		ret += ft_power(len_base, pow) * (indice_in_base(str[i],base));
+		i--;
+		pow++;
+	}
+	return ret * neg;
 }
 
 int main()
 {
-	printf("%d\n", ft_atoi_base("testsadaf", "es+t"));
+	printf("== %d\n", ft_atoi_base(" \t\r--++-+-2a", "0123456789abcdef"));
+	printf("== %d\n", ft_atoi_base(" \t\r--++-+-101010", "01"));
+	printf("== %d\n", ft_atoi_base(" \t\r--++-+-vn", "poneyvif"));
 }
